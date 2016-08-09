a69f05fb669105201b0fdab736731f09158d1aeb
James <james@team-wombat.com>
2016-08-09 14:17:55 -0400

    Using window.game to initiate game in a single state

--- public/js/states/play.controller.js
+++ public/js/states/play.controller.js
@@ -18,6 +18,16 @@
         //            var gameFile = $injector.get('game');
         //        });
 
+        window.game = new Phaser.Game(800, 600, Phaser.AUTO, '');
+
+        game.state.add('boot', bootState);
+        game.state.add('load', loadState);
+        game.state.add('menu', menuState);
+        game.state.add('play', playState);
+        game.state.add('win', winState);
+        game.state.add('lose', loseState);
+
+        game.state.start('boot');
 
     }
 })();
