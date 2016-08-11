ed6ee5c393ca3a831b2ee98db82ed25200fbf5a0
James <james@team-wombat.com>
2016-08-11 17:09:50 -0400

    this is falling apart. hopes destroyed.

--- public/js/app.js
+++ public/js/app.js
@@ -19,13 +19,13 @@
     function RouterFunction($stateProvider, $locationProvider, $urlRouterProvider) {
         $locationProvider.html5Mode(true)
         $stateProvider
-            .state("root", {
-                url: "/",
-                template: "",
-                // controller: function($state, user) {
-                //     if ($state.is("root")) $state.go(user.loggedIn ? "Menu" : "Splash");
-                // }
-            })
+            // .state("root", {
+            //     url: "/",
+            //     template: "",
+            //     // controller: function($state, user) {
+            //     //     if ($state.is("root")) $state.go(user.loggedIn ? "Menu" : "Splash");
+            //     // }
+            // })
             .state("Splash", {
                 url: "/splash",
                 templateUrl: "/assets/js/states/splash.html",
