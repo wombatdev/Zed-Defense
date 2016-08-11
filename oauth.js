96d452e48920f17795e504c277c6210a8a909bf4
James <james@team-wombat.com>
2016-08-11 13:52:59 -0400

    Changing callback url

--- oauth.js
+++ oauth.js
@@ -2,7 +2,7 @@ var ids = {
   facebook: {
     clientID: '1738032826451393',
     clientSecret: '393cd966989b6260ed2ae0de213021be',
-    callbackURL: 'http://worldofzed.herokuapp.com/signup/facebook/return'
+    callbackURL: 'http://worldofzed.herokuapp.com/fbreturn.html?status=success'
   }
 };
 
